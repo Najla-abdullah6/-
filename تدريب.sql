671c43ff6eb4d81daf590f7f928fdf7cc77e792a
Najla-abdullah6 <najlaa.abdullah6@gmail.com>
2026-08-08 21:32:41 +0300

    Add files via upload

---
+++ تدريب.sql
@@ -0,0 +1,10 @@
+CREATE TABLE 'najlaa'.'castemer'(
+id_name INT NOT NULL,
+'cast_name' VARCHAR(45)NOT NULL,
+'castphon'VARCHAR(45)NOT NULL,
+'castAddres'VARCHAR(45) NOT NULL,
+PRIMARY KEY('id_cast'));
+
+
+
+SELECT *FROM CASTEMER
